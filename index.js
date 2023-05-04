6895766c64ed013fcc8e3fe31f650e28a8340b24
Hasib <hhasibul231@gmail.com>
2023-05-04 16:49:04 +0600

    chef api work perfectly

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
+const chef = require('./data/chef.json');
+
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send('Hello from my first ever server');
+    res.send('chef is running');
 })
 
-app.get('/data', (req, res) => {
-    res.send('more data comming soon');
-})
+app.get("/chef", (req, res) => {
+  res.send(chef);
+});
+
 
 app.listen(port, () => {
     console.log(`My first server is running on port: ${port}`);
